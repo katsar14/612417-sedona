bfd3b312b325ddbf69aaea824d79bbe80b54039e
katsar14 <36961391+katsar14@users.noreply.github.com>
2018-03-27 15:51:10 +0300

    добавляет JS

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 var button = document.querySelector(".search-btn-toggle");
 var modalSearch = document.querySelector(".modal-search");
-var checkIn = modalSearch.querySelector("[name='checkin-date']")
+var form = document.querySelector(".search-form");
+var checkIn = modalSearch.querySelector("input[name='checkin-date']");
+var checkOut = modalSearch.querySelector("input[name='checkout-date']");
+var adultNumber = modalSearch.querySelector("input[name='adult-guests-number']");
+var childrenNumber = modalSearch.querySelector("input[name='children-guests-number']");
 
 button.addEventListener("click", function(evt){
   evt.preventDefault();
   modalSearch.classList.toggle("modal-search-show");
-  checkIn.focus();
+});
+
+form.addEventListener("submit", function(evt){
+    if(!checkIn.value || !checkOut.value || !adultNumber || !childrenNumber) {
+        evt.preventDefault();
+    }
 });
